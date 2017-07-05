a751f52a3bfaf729e4fb59b3b131fa8abf5ff61c
Razor Tag <razortag95@gmail.com>
2017-07-04 20:34:27 -0700

    Adding a dummy endpoint.

---
+++ complete/src/main/java/hello/DefaultValues.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class DefaultValues {
+
+	public static final String FIRSTNAME="Jim";
+	public static final String LASTNAME="Baker";
+	public static final String USERID="jimbaker";
+	public static final Long EMPID=1l;
+	
+}
